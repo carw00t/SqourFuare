aae1460c62e027d0e1a91f1ef1d4ea3102a92d2b
Jacob Van De Weert <vandeweertj@gmail.com>
2014-02-24 01:36:51 -0500

    signups and logins now work with parse :D we have data

--- SqourFare/SFLoginViewController.m
+++ SqourFare/SFLoginViewController.m
@@ -7,6 +7,7 @@
 //
 
 #import "SFLoginViewController.h"
+#import <Parse/Parse.h>
 
 @interface SFLoginViewController ()
 @property (strong, nonatomic) IBOutlet UITextField *usernameTextField;
@@ -48,11 +49,71 @@
 }
 
 - (IBAction)signUpButton:(id)sender {
-  NSLog(@"User %@ signed up with password %@", self.usernameTextField.text, self.passwordTextField.text);
+  
+  if ([self.usernameTextField.text isEqualToString:@"Username"] ||
+      [self.usernameTextField.text isEqualToString:@""] ||
+      [self.passwordTextField.text isEqualToString:@"Password"] ||
+      [self.passwordTextField.text isEqualToString:@""]) {
+    NSLog(@"Oh, real original (put an alert to enter something valid here)");
+  }
+  else {
+    PFObject *signupObj = [PFObject objectWithClassName:@"User"];
+    [signupObj setObject:self.usernameTextField.text forKey:@"username"];
+    [signupObj setObject:self.passwordTextField.text forKey:@"password"];
+    [signupObj save];
+    
+    PFQuery *dupCheck = [PFQuery queryWithClassName:@"User"];
+    [dupCheck whereKey:@"username" equalTo:self.usernameTextField.text];
+    [dupCheck whereKey:@"password" equalTo:self.passwordTextField.text];
+    NSArray *users = [dupCheck findObjects];
+    
+    if ([users count] == 0) {
+      NSLog(@"Error signing up. Try again later. (popup message)");
+    }
+    else if ([users count] == 1) {
+      [self.loginDelegate userLoggedInWithUsername:self.usernameTextField.text
+                                          password:self.passwordTextField.text];
+    }
+    else if ([users count] > 1) {
+      NSLog(@"Duplicate user (should also be a popup message");
+      PFObject *earliest;
+      NSDate *earliestDate;
+      
+      for (PFObject *user in users) {
+        if (earliest == nil || [earliestDate compare:user[@"createdAt"]] == NSOrderedAscending) {
+          earliest = user;
+          earliestDate = user[@"createdAt"];
+        }
+      }
+
+      NSMutableArray *muteUsers = [NSMutableArray arrayWithArray:users];
+      [muteUsers removeObjectIdenticalTo:earliest];
+      [PFObject deleteAllInBackground:muteUsers];
+    }
+  }
 }
 
 - (IBAction)loginButton:(id)sender {
-  [self.loginDelegate userLoggedInWithUsername:self.usernameTextField.text password:self.passwordTextField.text];
   
+  if ([self.usernameTextField.text isEqualToString:@"Username"] ||
+      [self.usernameTextField.text isEqualToString:@""] ||
+      [self.passwordTextField.text isEqualToString:@"Password"] ||
+      [self.passwordTextField.text isEqualToString:@""]) {
+    NSLog(@"Oh, real original (put an alert to enter something valid here)");
+  }
+  else {
+    PFQuery *findUser = [PFQuery queryWithClassName:@"User"];
+    [findUser whereKey:@"username" equalTo:self.usernameTextField.text];
+    [findUser whereKey:@"password" equalTo:self.passwordTextField.text];
+    
+    if ([findUser getFirstObject]) {
+      [self.loginDelegate userLoggedInWithUsername:self.usernameTextField.text
+                                          password:self.passwordTextField.text];
+    }
+    else {
+      NSLog(@"Invalid password or user not found. (popup)");
+    }
+  }
 }
+
 @end
